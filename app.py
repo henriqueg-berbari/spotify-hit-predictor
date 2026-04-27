d1d1b1bb427842171fb0059b9f54e0598fe049d6
henriqueg-berbari <henriquegberbari@gmail.com>
2026-04-27 12:38:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -420,9 +420,9 @@ with tab_conclusions:
     # --- 4. FINAL SUMMARY ---
     st.divider()
     st.success("""
-    **Project Conclusion:** This analysis proves that while data provides a stable 'floor' 
-    for success, the 'ceiling' is determined by human connection and cultural timing. 
-    This model acts as a powerful **Risk Mitigation** tool for any music-focused team.
+    **Project Conclusion:** This analysis demonstrates that while data provides a stable 'floor' for success by identifying market trends, the 'ceiling' is ultimately determined by human connection and cultural timing. This model serves as a powerful Risk Mitigation tool for industry professionals—offering a map of the landscape, but not the destination. 
+    This model acts as a powerful **Risk Mitigation** tool for any music-focused team, **but shouldnt be a single source of truth, since music is art, and arts shouldnt be quantifiable**.
+    Ultimately, while we can quantify the patterns of the past, the "magic" of a hit remains a human element that data can observe, but never truly replicate. **Music is an art first; the data is simply its echo**.
     """)
     
     st.balloons()
